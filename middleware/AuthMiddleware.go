10cc54eb620fe4ed265005b640834dc0ae9e34b0
LSunQQ <563631596@qq.com>
2024-03-04 12:03:22 +0800

    add middleware and jwt

---
+++ middleware/AuthMiddleware.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"ginEssential/common"
+	"ginEssential/model"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// 获取authorization header
+		tokenString := ctx.GetHeader("Authorization")
+
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "token为空前缀有误 权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString = tokenString[7:]
+
+		token, claims, err := common.ParseToken(tokenString)
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "解析token失败 权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		// 验证通过后获取claims中的userId
+		userId := claims.UserId
+		DB := common.GetDB()
+		var user model.User
+		DB.First(&user, userId)
+
+		// 用户
+		if user.ID == 0 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "用户不存在 权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		// 用户存在 将user信息写入上下文
+		ctx.Set("user", user)
+
+		ctx.Next()
+
+	}
+}
--- routes.go
+++ routes.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"ginEssential/controller"
+	"ginEssential/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/login", controller.Login)
+	r.GET("api/auth/info", middleware.AuthMiddleware(), controller.Info)
 
 	// r.GET("/ping", func(c *gin.Context) {
 	// 	c.JSON(200, gin.H{
